372c8bbf7448866cae7fffe2b91d44043e9daa43
Ian Coleman <ian@iancoleman.io>
2018-01-23 13:21:33 +1100

    Remove unused safecoin file

--- src/safenet/safecoin.go
+++
@@ -1,9 +0,0 @@
-package safenet
-
-const coinNameByteLen = 4 // 4 bytes * 8 bits per byte = 32 bits = 2^32 coins
-
-func randomSafecoinAddress() string {
-	coinname := make([]byte, coinNameByteLen)
-	prng.Read(coinname)
-	return string(coinname)
-}
